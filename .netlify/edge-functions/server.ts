954f17f0bd2cbecb61fc8627cd5b90a74487b508
Nick Taylor <nick@iamdeveloper.com>
2022-07-04 21:57:28 -0400

    chore: cleaned up some dead/test code

--- .netlify/edge-functions/server.ts
+++ .netlify/edge-functions/server.ts
@@ -7,15 +7,8 @@
 import { start } from "$fresh/server.ts";
 import manifest from "../../fresh.gen.ts";
 
-export default async function() {
-  // try {
-    console.log('yolo')
-    await start(manifest);
-  // } catch (error) {
-  //   return new Response(JSON.stringify(error.message, null, 2), { status: 500 });
-  // }
-}
+export default async function () {
+  console.log("manifest", manifest);
 
-// export default () => {
-//   return new Response(JSON.stringify(manifest, null, 2))
-// }
+  await start(manifest);
+}
