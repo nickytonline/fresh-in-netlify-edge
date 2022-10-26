d3d37e53f1a591dbe2257909f393339468aa916b
Nick Taylor <nick@iamdeveloper.com>
2022-10-26 18:20:27 -0400

    chore: upgraded fresh via deno run -A -r https://fresh.deno.dev/update .

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,6 +2,7 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
+import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/index.tsx";
@@ -17,6 +18,7 @@ const manifest = {
     "./islands/Counter.tsx": $$0,
   },
   baseUrl: import.meta.url,
+  config,
 };
 
 export default manifest;
